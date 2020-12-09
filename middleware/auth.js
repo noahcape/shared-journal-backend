abef2c26ebc8bfffe5034f50fc32fcab0e84d028
Noah <noahcape@gmail.com>
2020-12-09 11:10:45 -0500

    console.log test

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,9 +11,9 @@ const auth = (req, res, next) => {
 
         if (!verifiedToken)
             return res.status(401).json({ msg: "Token verification failed, authorization denied" })
-
+        console.log("maybe req.user")
         req.user = verifiedToken.id
-
+        console.log("not request.user")
         next();
     } catch (err) {
         res.status(500).json({ error: err.message })
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -83,10 +83,10 @@ router.get("/getDateOptions", auth, async (req, res) => {
 })
 
 // get by id
-// router.get("/getById", auth, async (req, res) => {
-//     const post = await Post.findById(req.query.id).catch(err => { console.error(err) });
-//     res.json(post);
-// })
+router.get("/getById", auth, async (req, res) => {
+    const post = await Post.findById(req.query.id).catch(err => { console.error(err) });
+    res.json(post);
+})
 
 // delete photo 
 router.delete("/deleteImage", auth, async (req, res) => {
@@ -122,6 +122,7 @@ router.delete("/deleteImage", auth, async (req, res) => {
 
 // delete by id in query
 router.delete("/:id", auth, async (req, res) => {
+    console.log("Got it here must not me auth")
     const post = await Post.findById(req.params.id)
 
     post.image_keys.forEach(key => {
