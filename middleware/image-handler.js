d14ad8f9ec8e67d4129c4b58894ac0c28b049b57
Noah Cape <69659089+noahcape@users.noreply.github.com>
2022-12-25 12:09:23 -0600

    Update image-handler.js

--- middleware/image-handler.js
+++ middleware/image-handler.js
@@ -4,6 +4,19 @@ const multerS3 = require('multer-s3');
 
 const s3 = new aws.S3();
 
+aws.config.update(
+    {
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        region: 'us-west-2',
+    },
+    (err) => {
+        if (err) console.log(err);
+
+        console.log('AWS s3 set up');
+    },
+);
+
 const upload = multer({
   storage: multerS3({
     s3,
