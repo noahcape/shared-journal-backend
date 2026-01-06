2b5b9be484081d22bab6d9ffa4fdf652dbe590d2
Robbie Cape <robbie@capefamily.com>
2026-01-05 17:59:42 -0800

    Add manual trigger endpoint for monthly emails

--- routes/stayAwakeRoute.js
+++ routes/stayAwakeRoute.js
@@ -1,8 +1,34 @@
 const router = require("express").Router();
+const getUsers = require("../middleware/queryUsers");
+const compileUpdates = require("../middleware/compileUpdates");
 
 router.get("/", (req, res) => {
   console.log("i'm awake");
   res.send("stay awake");
+});
+
+// Manual trigger for monthly email send
+router.get("/triggerMonthlyEmail", async (req, res) => {
+  const { secret } = req.query;
+
+  // Simple auth check - use JWT_PASSWORD as the secret
+  if (secret !== process.env.JWT_PASSWORD) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  console.log("Manual trigger: sending monthly emails");
+
+  try {
+    const users = await getUsers();
+    const userIds = users.map((user) => user._id);
+
+    compileUpdates(userIds);
+
+    res.send({ result: "Monthly email send triggered", userCount: userIds.length });
+  } catch (e) {
+    console.error("Error triggering monthly email:", e);
+    res.status(500).send({ error: e.message });
+  }
 });
 
 module.exports = router;
